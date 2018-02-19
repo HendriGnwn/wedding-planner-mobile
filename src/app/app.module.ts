bb938fbff07edc403ddf27a29a4beb78c56b2e31
Hendri <hendri.gnw@gmail.com>
2018-02-19 15:12:30 +0700

    add welcome page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { LoginPage } from '../pages/login/login';
+import { WelcomePage } from '../pages/welcome/welcome';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-	LoginPage
+	  LoginPage,
+    WelcomePage
   ],
   imports: [
     BrowserModule,
@@ -22,7 +24,8 @@ import { LoginPage } from '../pages/login/login';
   entryComponents: [
     MyApp,
     HomePage,
-	LoginPage
+	  LoginPage,
+    WelcomePage
   ],
   providers: [
     StatusBar,
